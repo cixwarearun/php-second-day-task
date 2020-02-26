61fc73795ca9d603224bceb7886c0392ef515306
Arun Bastola <cixwarearun@gmail.com>
2020-02-26 10:42:15 +0545

    committed

---
+++ evenOdd.php
@@ -0,0 +1,18 @@
+<?php
+  $n=11;
+  echo 'let n=11';
+  echo '<br>';
+  if($n % 2 == 0)
+  {
+
+        echo 'Number is even';
+  }
+
+  else
+      {
+        echo 'Number is odd';
+      }
+        
+
+
+?>
---
+++ factorialOfNumber.php
@@ -0,0 +1,8 @@
+<?php
+  $n=5;
+  $factorial=1;
+  for($i=1; $i<$n; $i++){
+      $factorial = $factorial * $i;
+  }
+  echo  'Factorial of  5 is ' .$factorial;
+?>

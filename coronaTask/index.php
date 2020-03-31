cb997053b7cdf7e62fea9e55f48952e421ba0a1f
Arun Bastola <cixwarearun@gmail.com>
2020-03-31 12:01:07 +0545

    header part of the task is completed

---
+++ coronaTask/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Corona Task</title>
+    <link rel="stylesheet" href="tailwind.css">
+    <link rel="stylesheet" href="  https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+</head>
+<body>
+    <header class="bg-blue-700">
+        <div class="flex justify-between  text-white mx-48 pt-2 pb-2">
+            <div class="flex text-center">
+                <div class="ml-2">
+                <img src="png/nepal.png" alt="nepal flag"> 
+                </div>
+                <div class="ml-2 text-xs">
+                    ▼     
+                </div>
+                <div class="ml-4">
+                <i class="fas fa-phone-alt"></i>
+                </div>
+                <div class="ml-2">+44 345 4030 767 / +27 21 528 3420</div>
+            </div>
+            <div class="flex">
+                <div>Login</div>   
+                <div class="ml-2">
+                <i class="fas fa-user-circle"></i>
+                </div>    
+                <div class="ml-2  outline-none"> 
+                    <input  class="pl-2 rounded  h-6 text-blue-700  overflow-hidden  "  placeholder="Search..."  type="text" required>
+                </div>
+                <div class="ml-2">
+                    <i class="fas fa-search  "></i>
+                </div>           
+            </div>
+        </div>
+    </header>
+    <section >
+        <div class="mx-48 text-gray-800 flex justify-between mt-2 ">
+            <div>
+                 <img class="h-20" src="png/a.png" alt="fly">
+            </div>
+            <div class="pt-4">
+                <a class="mr-8 hover:text-blue-700" href="solution.html">Solution <span class="text-xs">▼</span></a>
+                <a class="mr-8 hover:text-blue-700" href="developers.html">Developers <span class="text-xs">▼</span></a>
+                <a class="mr-8 hover:text-blue-700" href="pricing.html">Pricing</a>
+                <a class="mr-8 hover:text-blue-700" href="resources.html">Resources <span class="text-xs">▼</span></a>
+                <a class="mr-8 hover:text-blue-700" href="company.html">Company</a>
+                <a class="mr-8 rounded-md bg-blue-700 text-white px-2 py-2  hover:bg-red-700" href="contact.html">Contact Us</a>
+            </div>
+        </div>
+       
+    </section>
+    
+</body>
+</html>
